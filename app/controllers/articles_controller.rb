faccac96af532cb86ae6ff48a41b64f607c4f140
Shruti Gupta <shruti.gupta@poplify.com>
2022-08-03 19:15:44 +0530

    Associate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)      # DRY code
+        @article.user = User.first
         # render plain: @article.inspect
         if @article.save
             flash[:notice] = "Article was created successfully."
